6898d788ed40f47ea26c803523669a3ba377b518
tshibangumukadi83-oss <tshibangumukadi83@gmail.com>
2026-07-05 13:58:49 +0200

    Implement navigation menu toggle functionality

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,1 +1,16 @@
+// scripts/navigation.js
 
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+
+    if (navigation.classList.contains("open")) {
+        menuButton.innerHTML = "&times;";
+        menuButton.setAttribute("aria-label", "Close Navigation Menu");
+    } else {
+        menuButton.innerHTML = "&#9776;";
+        menuButton.setAttribute("aria-label", "Open Navigation Menu");
+    }
+});
