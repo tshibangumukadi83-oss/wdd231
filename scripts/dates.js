9befc0d2c0354d3881f25754f8e9cf3419eba39a
tshibangumukadi83-oss <tshibangumukadi83@gmail.com>
2026-07-05 14:00:08 +0200

    Add current year and last modified date display

--- scripts/dates.js
+++ scripts/dates.js
@@ -1,1 +1,15 @@
+// scripts/dates.js
 
+// Afficher automatiquement l'année actuelle
+const currentYear = document.querySelector("#currentyear");
+
+if (currentYear) {
+    currentYear.textContent = new Date().getFullYear();
+}
+
+// Afficher la date de dernière modification de la page
+const lastModified = document.querySelector("#lastModified");
+
+if (lastModified) {
+    lastModified.textContent = `Last Modification: ${document.lastModified}`;
+}
