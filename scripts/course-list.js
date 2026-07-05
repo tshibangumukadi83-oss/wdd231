3d8fe9abeba60334bca62cd2a6b94b9550919849
tshibangumukadi83-oss <tshibangumukadi83@gmail.com>
2026-07-05 14:01:29 +0200

    Add course list to course-list.js
    Added a list of courses with details such as subject, number, title, credits, certificate, and completion status.

--- scripts/course-list.js
+++ scripts/course-list.js
@@ -1,1 +1,60 @@
+// scripts/course-list.js
 
+const courses = [
+    {
+        subject: "CSE",
+        number: 110,
+        title: "Introduction to Programming",
+        credits: 2,
+        certificate: "Web and Computer Programming",
+        completed: true
+    },
+    {
+        subject: "WDD",
+        number: 130,
+        title: "Web Fundamentals",
+        credits: 2,
+        certificate: "Web and Computer Programming",
+        completed: true
+    },
+    {
+        subject: "CSE",
+        number: 111,
+        title: "Programming with Functions",
+        credits: 2,
+        certificate: "Web and Computer Programming",
+        completed: false
+    },
+    {
+        subject: "WDD",
+        number: 131,
+        title: "Dynamic Web Fundamentals",
+        credits: 2,
+        certificate: "Web and Computer Programming",
+        completed: true
+    },
+    {
+        subject: "WDD",
+        number: 231,
+        title: "Web Frontend Development I",
+        credits: 2,
+        certificate: "Web and Computer Programming",
+        completed: false
+    },
+    {
+        subject: "CSE",
+        number: 210,
+        title: "Programming with Classes",
+        credits: 2,
+        certificate: "Web and Computer Programming",
+        completed: false
+    },
+    {
+        subject: "WDD",
+        number: 330,
+        title: "Web Frontend Development II",
+        credits: 2,
+        certificate: "Web and Computer Programming",
+        completed: false
+    }
+];
