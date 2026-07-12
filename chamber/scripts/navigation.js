f24648424cc57ca6adb8c031cea32e9d0d31fd16
tshibangumukadi83-oss <tshibangumukadi83@gmail.com>
2026-07-12 16:53:43 +0200

    Add toggle functionality for navigation menu

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -1,1 +1,8 @@
 
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector("#nav");
+
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    menuButton.classList.toggle("open");
+});
