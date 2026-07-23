a570cb4ab17f7a638d9aa4bccaba86b6c3e12a87
tshibangumukadi83-oss <tshibangumukadi83@gmail.com>
2026-07-23 16:44:34 +0200

    Add current date and time to timestamp field

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,1 +1,11 @@
+// Set current date and time in hidden timestamp field
 
+const timestamp = document.querySelector("#timestamp");
+
+if (timestamp) {
+
+    const now = new Date();
+
+    timestamp.value = now.toLocaleString();
+
+}
